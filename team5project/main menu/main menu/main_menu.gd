19e4b436b159988a2ceead67f4ec7e1af28bba67
qdvo2503 <qdvo@knox.edu>
2024-02-27 14:23:02 -0600

    Add credit scene

--- team5project/main menu/main menu/main_menu.gd
+++ team5project/main menu/main menu/main_menu.gd
@@ -7,8 +7,9 @@ extends Control
 #@onready var instruction_button = $MarginContainer/HBoxContainer/VBoxContainer/Instruction_Button as Button
 #@onready var start_level = preload("res://mainGame.tscn") as PackedScene
 @onready var settings_menu = $Settings_Menu as SettingsMenu
+@onready var credits_menu = $Credits_Menu as CreditsMenu
 @onready var margin_container = $MarginContainer as MarginContainer
-
+@onready var credits_button = $MarginContainer/HBoxContainer/VBoxContainer/Credits_Button as Button
 func _ready():
 	handling_connecting_signals()
 	
@@ -23,10 +24,19 @@ func on_exit_settings_menu() -> void:
 	margin_container.visible = true
 	settings_menu.visible = false
 
+func on_exit_credits_menu() -> void:
+	margin_container.visible = true
+	credits_menu.visible = false
+	
 func on_settings_pressed() -> void:
 	margin_container.visible =  false
 	settings_menu.set_process(true)
 	settings_menu.visible = true
+
+func on_credits_pressed() -> void:
+	margin_container.visible =  false
+	credits_menu.set_process(true)
+	credits_menu.visible = true
 
 func on_instruction_pressed() -> void:
 	pass
@@ -35,4 +45,6 @@ func handling_connecting_signals() -> void:
 	start_button.button_down.connect(on_start_pressed)
 	settings_button.button_down.connect(on_settings_pressed)
 	exit_button.button_down.connect(on_exit_pressed)
+	credits_button.button_down.connect(on_credits_pressed)
 	settings_menu.exit_settings_menu.connect(on_exit_settings_menu)
+	credits_menu.exit_credits_menu.connect(on_exit_credits_menu)
