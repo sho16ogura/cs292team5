f3f7c2df7d9b722be43d731c3dc581dfa9a61893
sho16ogura <sho16_og@icloud.com>
2024-03-07 13:28:07 -0600

    final bug fix
    no pause in tutorial

--- team5project/pause_menu.gd
+++ team5project/pause_menu.gd
@@ -10,7 +10,7 @@ func _ready():
 	handling_connecting_signals()
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
+	if event.is_action_pressed("pause") and Global.tutorial_count == -1:
 		_is_paused = !_is_paused
 	
 func set_paused(value: bool) -> void:
